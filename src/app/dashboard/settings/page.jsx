6a7bf2f3857613681cf02aa2e73eae16168b93ce
Bazi <bazistudio51@gmail.com>
2025-12-04 14:11:18 +0500

    update all files

--- src/app/dashboard/settings/page.jsx
+++ src/app/dashboard/settings/page.jsx
@@ -2,14 +2,14 @@
 
 
 
-export default function sPage() {
+export default function SettingsPage() {
   return (
     <div className="flex h-screen bg-background text-foreground">
       
       <div className="flex-1 flex flex-col overflow-hidden">
           <main className="flex-1 p-6 overflow-auto">
-          <h1 className="text-3xl font-bold mb-4">s</h1>
-          {/* Your s content goes here */}
+          <h1 className="text-3xl font-bold mb-4">Settings</h1>
+          {/* Your s Settings goes here */}
         </main>
       </div>
     </div>
